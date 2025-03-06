71c36dcd1d200917171b60904dbd853493fce16d
Tolrom <max.le.morlot@gmail.com>
2025-03-06 09:25:21 +0100

    users display

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
+    #[Route(path: "/users", name: "app_user_list")]
+    public function allAccounts(EntityManagerInterface $em): Response
+    {
+        $users = $em->getRepository(Account::class)->findAll();
+        return $this->render('users.html.twig', [
+            'users' => $users
+        ]);
+    }
 
     #[Route(path: "/register", name: "app_user_register")]
     public function register(): Response
